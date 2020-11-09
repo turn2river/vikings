fd68aeb3b3016a1a8a75bf8afa96ad775d76fbfe
alexey.krylov <turn2river@gmail.com>
2020-11-09 17:09:37 +0300

    adds bottom slider, script for it

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ $(function () {
     youtube: {
       controls: 0,
       showinfo: 0,
-    }
+    },
+  });
+
+  $(".heroes__slider-img").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,        
+    asNavFor: ".heroes__slider-text",
+    prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+  });
+  $(".heroes__slider-text").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: ".heroes__slider-img",    
+    arrows: false,
   });
 });
