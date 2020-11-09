0c89ab4b26e00ac784740acf595e85afffb9f4cf
alexey.krylov <turn2river@gmail.com>
2020-11-09 21:02:32 +0300

    modifies slick script (defered) + footer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(function () {
   $("[data-fancybox]").fancybox({
     youtube: {
-      controls: 0,
+      controls: 1,
       showinfo: 0,
     },
   });
 
   $(".heroes__slider-img").slick({
     slidesToShow: 1,
-    slidesToScroll: 1,        
+    slidesToScroll: 1,
     asNavFor: ".heroes__slider-text",
     prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
     nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
@@ -17,7 +17,7 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: true,
-    asNavFor: ".heroes__slider-img",    
+    asNavFor: ".heroes__slider-img",
     arrows: false,
   });
 });
